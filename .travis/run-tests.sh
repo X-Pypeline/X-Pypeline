43d241e53b6b1ac79bd978537d77adda400e626f
Scott Coughlin <scott.coughlin@ligo.org>
2019-02-28 08:14:02 -0800

    Updating the command lines that need to be checked in the CI

--- .travis/run-tests.sh
+++ .travis/run-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 coverage run ./setup.py test
-coverage run --append `which setUpJobs` --help
+coverage run --append `which xpipeline-workflow` --help
+coverage run --append `which xpipeline-analysis` --help
