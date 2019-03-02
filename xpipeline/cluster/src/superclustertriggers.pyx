41d1ae3fcfb1622fb867e2c8e462bd6c2263fb49
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2019-03-02 16:33:25 +0000

    Need to provide flatten array of the bounding box to the function I guess

--- xpipeline/cluster/src/superclustertriggers.pyx
+++ xpipeline/cluster/src/superclustertriggers.pyx
@@ -9,10 +9,10 @@ cdef extern from "fastsupercluster.h" nogil:
 
 def fastsupercluster_wrapper(ntriggers, object rectangles,):
     cdef:
-        np.ndarray[np.double_t, ndim=2, mode='c'] rectangles_tmp
+        np.ndarray[np.double_t, ndim=1, mode='c'] rectangles_tmp
         np.ndarray[np.double_t, ndim=1, mode='c'] mask = np.zeros((ntriggers,), dtype=np.double)
 
-    rectangles_tmp = np.ascontiguousarray(rectangles, dtype=np.float64)
+    rectangles_tmp = np.ascontiguousarray(rectangles.flatten('F'), dtype=np.float64)
 
-    fastsupercluster(ntriggers, &rectangles_tmp[0,0], &mask[0])
+    fastsupercluster(ntriggers, &rectangles_tmp[0], &mask[0])
     return mask
