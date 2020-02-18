30bca53d74a835fd76bb0573c2d20e4a4b99fded
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2020-02-18 09:22:14 -0600

    Minor syntax change

--- examples/snews/xpypeline-setup-on-cit.sh
+++ examples/snews/xpypeline-setup-on-cit.sh
@@ -1,3 +1,4 @@
+source /cvmfs/oasis.opensciencegrid.org/ligo/sw/conda/etc/profile.d/conda.sh
 source ~psutton/opt/xpipeline/r5650_matlab2015a_localMCR/bin/envsetup.sh 
 source activate /home/scott.coughlin/.conda/envs/xpipeline-py36/
 xpipeline-workflow --search-type grb --params-file grb_mini.ini -n GRB160830 -g 1128477617 -r 307.65 -d 45.72  -i H1 L1 --FAP 99.99 --catalogdir /home/scoughlin/PhD/src/xpipeline-waveforms/  --off-source-inj
